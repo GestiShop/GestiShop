5b39954c346e14624332aafe3de9716581914009
FerranMontoliu <ferran.montoliu@students.salle.url.edu>
2021-06-06 19:57:15 +0200

    Added settings module

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,9 +1,118 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Box from '@material-ui/core/Box'
+import { useTranslation } from 'react-i18next'
+import ConfigLanguage from './settings-module/ConfigLanguage'
+import ConfigCurrencyInfo from "./settings-module/ConfigCurrencyInfo"
+import ConfigBusinessInfo from "./settings-module/ConfigBusinessInfo"
+import ConfigDbInfo from "./settings-module/ConfigDbInfo"
+import ConfigUsers from "./settings-module/ConfigUsers"
+import ConfigInputMode from "./settings-module/ConfigInputMode"
+import ConfigModules from "./settings-module/ConfigModules"
+import ConfigAdvanced from "./settings-module/ConfigAdvanced"
 
-const Settings = () => {
+function TabPanel(props) {
+    const {children, value, index, ...other} = props
+
     return (
-        <p>Not implemented >:(</p>
-    )
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    {children}
+                </Box>
+            )}
+        </div>
+    );
 }
 
-export default Settings
+function a11yProps(index) {
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+        display: 'flex',
+        height: '100%',
+    },
+    tabs: {
+        borderRight: `1px solid ${theme.palette.divider}`,
+    },
+}));
+
+export default function Settings() {
+    const {t} = useTranslation()
+
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div className={classes.root}>
+            <Tabs
+                orientation="vertical"
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+                aria-label="Vertical tabs example"
+                className={classes.tabs}
+            >
+                <Tab label={t('settings.language_config.language')} {...a11yProps(0)} />
+                <Tab label={t('settings.currency_config.currency')} {...a11yProps(1)} />
+                <Tab label={t('settings.business_config.business')} {...a11yProps(2)} />
+                <Tab label={t('settings.database_config.database')} {...a11yProps(3)} />
+                <Tab label={t('settings.users_config.users')} {...a11yProps(4)} />
+                <Tab label={t('settings.input_mode_config.input_mode')} {...a11yProps(5)} />
+                <Tab label={t('settings.modules_config.modules')} {...a11yProps(6)} />
+                <Tab label={t('settings.advanced_config.advanced')} {...a11yProps(7)} />
+            </Tabs>
+
+            <TabPanel value={value} index={0}>
+                <ConfigLanguage/>
+            </TabPanel>
+
+            <TabPanel value={value} index={1}>
+                <ConfigCurrencyInfo/>
+            </TabPanel>
+
+            <TabPanel value={value} index={2}>
+                <ConfigBusinessInfo/>
+            </TabPanel>
+
+            <TabPanel value={value} index={3}>
+                <ConfigDbInfo/>
+            </TabPanel>
+
+            <TabPanel value={value} index={4}>
+                <ConfigUsers/>
+            </TabPanel>
+
+            <TabPanel value={value} index={5}>
+                <ConfigInputMode/>
+            </TabPanel>
+
+            <TabPanel value={value} index={6}>
+                <ConfigModules/>
+            </TabPanel>
+
+            <TabPanel value={value} index={7}>
+                <ConfigAdvanced/>
+            </TabPanel>
+        </div>
+    );
+}
---
+++ src/components/settings-module/ConfigSummary.jsx
@@ -0,0 +1,95 @@
+import { createStyles, Divider, Grid, makeStyles } from '@material-ui/core'
+import React from 'react'
+
+import { useTranslation } from 'react-i18next'
+
+import { useSelector } from 'react-redux'
+
+
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        bold: {
+            fontWeight: 'bold'
+        }
+    })
+)
+
+export default function ConfigSummary() {
+    const {t} = useTranslation()
+
+    const defaultConfig = useSelector(store => store.configuration)
+
+    const classes = useStyles()
+
+    return (
+        <Grid container>
+            <Grid item xs={6}>
+                <p className={classes.bold}>{t('settings.language_config.language')}:</p>
+            </Grid>
+            <Grid item xs={6}>
+                <p>{defaultConfig.lang.label}</p>
+            </Grid>
+
+
+            <Grid item xs={12}><Divider/></Grid>
+
+
+            <Grid item xs={6}>
+                <p className={classes.bold}>{t('settings.currency_config.currency')}:</p>
+            </Grid>
+            <Grid item xs={6}>
+                <p>{defaultConfig.currency.label}</p>
+            </Grid>
+
+
+            <Grid item xs={12}><Divider/></Grid>
+
+
+            <Grid item xs={6}>
+                <p className={classes.bold}>{t('settings.business_config.name')}:</p>
+            </Grid>
+            <Grid item xs={6}>
+                <p>{defaultConfig.businessInfo.name}</p>
+            </Grid>
+
+            <Grid item xs={6}>
+                <p className={classes.bold}>{t('settings.business_config.nif')}:</p>
+            </Grid>
+            <Grid item xs={6}>
+                <p>{defaultConfig.businessInfo.nif}</p>
+            </Grid>
+
+
+            <Grid item xs={12}><Divider/></Grid>
+
+
+            <Grid item xs={6}>
+                <p className={classes.bold}>{t('settings.database_config.url')}:</p>
+            </Grid>
+            <Grid item xs={6}>
+                <p>{defaultConfig.databaseInfo.url}</p>
+            </Grid>
+
+            <Grid item xs={6}>
+                <p className={classes.bold}>{t('settings.database_config.port')}:</p>
+            </Grid>
+            <Grid item xs={6}>
+                <p>{defaultConfig.databaseInfo.port}</p>
+            </Grid>
+
+            <Grid item xs={6}>
+                <p className={classes.bold}>{t('settings.database_config.name')}:</p>
+            </Grid>
+            <Grid item xs={6}>
+                <p>{defaultConfig.databaseInfo.name}</p>
+            </Grid>
+
+            <Grid item xs={6}>
+                <p className={classes.bold}>{t('settings.database_config.user')}:</p>
+            </Grid>
+            <Grid item xs={6}>
+                <p>{defaultConfig.databaseInfo.user}</p>
+            </Grid>
+        </Grid>
+    )
+}
