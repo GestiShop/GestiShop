f6290d4a9aaff03a2379b252d689c9bfe8fb5370
FerranMontoliu <ferran.montoliu@students.salle.url.edu>
2021-06-20 12:40:15 +0200

    fix: fixed multi-lang not working + Added formik and yup dependencies

--- src/renderer.js
+++ src/renderer.js
@@ -30,5 +30,6 @@ window.require('dotenv').config()
 import * as ReactDOM from 'react-dom'
 import * as React from 'react'
 import App from './App'
+import './i18n'
 
 ReactDOM.render(<App/>, document.getElementById('root'))
