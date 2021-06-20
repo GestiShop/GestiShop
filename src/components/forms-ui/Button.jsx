4b3a95fe2902fbaef819c7f927e0de9d90d25bef
FerranMontoliu <ferran.montoliu@students.salle.url.edu>
2021-06-20 19:29:47 +0200

    feat: Added wrappers for Formik.

---
+++ src/components/forms-ui/Button.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+import { useFormikContext } from 'formik'
+
+const ButtonWrapper = ({children}) => {
+    const {submitForm} = useFormikContext()
+
+    const handleSubmit = () => {
+        submitForm()
+    }
+
+    const configButton = {
+        variant: 'contained',
+        color: 'primary',
+        fullWidth: true,
+        onClick: handleSubmit
+    }
+
+    return (
+        <Button {...configButton}>
+            {children}
+        </Button>
+    )
+}
+
+export default ButtonWrapper
