c7b717a45d566ef9a4d902e01ca41b63226c1f36
FerranMontoliu <ferran.montoliu@students.salle.url.edu>
2021-06-27 21:15:43 +0200

    feat: Translated accounting module appbar title.

--- src/components/accounting-module/AccountingModuleDashboard.jsx
+++ src/components/accounting-module/AccountingModuleDashboard.jsx
@@ -129,7 +129,7 @@ const AccountingModuleDashboard = () => {
                         <MenuIcon/>
                     </IconButton>
                     <Typography variant="h6" noWrap>
-                        Accounting module
+                        {t('accounting_module.accounting_module')}
                     </Typography>
                 </Toolbar>
             </AppBar>
