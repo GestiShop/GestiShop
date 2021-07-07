b49d5fbd5541cb8b2bf6690c56488ae2ab244974
FerranMontoliu <ferran.montoliu@students.salle.url.edu>
2021-07-07 20:15:17 +0200

    feat: Added styling to dialog.

--- src/components/ui/FullscreenDialog.jsx
+++ src/components/ui/FullscreenDialog.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import Button from '@material-ui/core/Button'
 import Dialog from '@material-ui/core/Dialog'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
@@ -8,8 +7,6 @@ import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
 import CloseIcon from '@material-ui/icons/Close'
 import Slide from '@material-ui/core/Slide'
-import CreateProduct from '../accounting-module/create/CreateProduct'
-import { useTranslation } from 'react-i18next'
 
 const useStyles = makeStyles((theme) => ({
     appBar: {
@@ -19,6 +16,9 @@ const useStyles = makeStyles((theme) => ({
         marginLeft: theme.spacing(2),
         flex: 1,
     },
+    contentContainer: {
+        padding: '1%'
+    }
 }))
 
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -26,32 +26,23 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 })
 
 const FullScreenDialog = ({open, closeCallback, title, childComponent}) => {
-    const { t } = useTranslation()
     const classes = useStyles()
 
-    const handleClose = (shouldCreate) => {
-        if (shouldCreate) {
-            // TODO: DO SOMETHING
-        }
-        closeCallback(shouldCreate)
-    }
-
     return (
-        <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+        <Dialog fullScreen open={open} onClose={closeCallback} TransitionComponent={Transition}>
             <AppBar className={classes.appBar}>
                 <Toolbar>
-                    <IconButton edge="start" color="inherit" onClick={() => handleClose(false)} aria-label="close">
+                    <IconButton edge="start" color="inherit" onClick={closeCallback} aria-label="close">
                         <CloseIcon/>
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
                         {title}
                     </Typography>
-                    <Button autoFocus color="inherit" onClick={() => handleClose(true)}>
-                        {t('buttons.create')}
-                    </Button>
                 </Toolbar>
             </AppBar>
-            {childComponent}
+            <div className={classes.contentContainer}>
+                {childComponent}
+            </div>
         </Dialog>
     )
 }
