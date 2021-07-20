6e004af918f26bc3d7e2342cb8ac8e84cac42119
FerranMontoliu <ferran.montoliu@students.salle.url.edu>
2021-07-20 02:36:45 +0200

    feat: Now the settings are a dialog inside the accounting module.

--- src/components/Onboarding.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Onboarding = () => {
-  return <p>Not done yet :)</p>;
-};
-
-export default Onboarding;
--- src/components/accounting-module/AccountingModuleDashboard.jsx
+++ src/components/accounting-module/AccountingModuleDashboard.jsx
@@ -1,35 +1,40 @@
 /* eslint-disable react/no-array-index-key */
-import React from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
+import { useTranslation } from 'react-i18next';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
+import { Link, useRouteMatch, Route, Switch, Redirect } from 'react-router-dom';
+import {
+  Drawer,
+  AppBar,
+  Toolbar,
+  List,
+  CssBaseline,
+  Typography,
+  Divider,
+  IconButton,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import SettingsIcon from '@material-ui/icons/Settings';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import EventIcon from '@material-ui/icons/Event';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
 import TimelineIcon from '@material-ui/icons/Timeline';
 import StoreIcon from '@material-ui/icons/Store';
 import CategoryIcon from '@material-ui/icons/Category';
-import { useTranslation } from 'react-i18next';
-import { Link, useRouteMatch, Route, Switch, Redirect } from 'react-router-dom';
 import ListProducts from './list/ListProducts';
 import ListTaxes from './list/ListTaxes';
 import ListUnitTypes from './list/ListUnitTypes';
 import ListWarehouses from './list/ListWarehouses';
 import Calendar from './calendar/Calendar';
 import ListCategories from './list/ListCategories';
+import FullScreenDialog from '../ui/FullscreenDialog';
+import Settings from '../settings-module/Settings';
 
 const DRAWER_WIDTH = 240;
 
@@ -98,11 +103,16 @@ const useStyles = makeStyles((theme) => ({
       overflow: 'auto',
     },
   },
+  navbar: {
+    justifyContent: 'space-between',
+  },
 }));
 
 const AccountingModuleDashboard = () => {
   const { t } = useTranslation();
   const { path, url } = useRouteMatch();
+
+  const [openSettingsDialog, setOpenSettingsDialog] = useState(false);
 
   const DRAWER_ITEMS = [
     [
@@ -154,106 +164,122 @@ const AccountingModuleDashboard = () => {
   };
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open,
-            })}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            {t('accounting_module.accounting_module')}
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}
-        classes={{
-          paper: clsx({
+    <>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar className={classes.navbar}>
+            <IconButton
+              color="inherit"
+              onClick={handleDrawerOpen}
+              edge="start"
+              className={clsx(classes.menuButton, {
+                [classes.hide]: open,
+              })}
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Typography variant="h6" noWrap>
+              {t('accounting_module.accounting_module')}
+            </Typography>
+
+            <IconButton
+              color="inherit"
+              edge="end"
+              onClick={() => setOpenSettingsDialog(true)}
+            >
+              <SettingsIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          className={clsx(classes.drawer, {
             [classes.drawerOpen]: open,
             [classes.drawerClose]: !open,
-          }),
-        }}
-      >
-        <div className={classes.toolbar}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? (
-              <ChevronRightIcon />
-            ) : (
-              <ChevronLeftIcon />
-            )}
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          {DRAWER_ITEMS.map((elementList, index) => {
-            let itemList = elementList.map((element) => (
-              <ListItem
-                button
-                key={element.text}
-                component={Link}
-                to={element.linkTo}
-                replace
-              >
-                <ListItemIcon>{element.icon}</ListItemIcon>
-                <ListItemText primary={element.text} />
-              </ListItem>
-            ));
-
-            if (index !== DRAWER_ITEMS.length - 1) {
-              itemList = itemList.concat(
-                <Divider key={`elementlist-${index}`} />
-              );
-            }
-
-            return itemList;
           })}
-        </List>
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <Switch>
-          <Route exact path={`${path}/calendar`}>
-            <Calendar />
-          </Route>
-          <Route exact path={`${path}/products`}>
-            <ListProducts />
-          </Route>
-          <Route exact path={`${path}/taxes`}>
-            <ListTaxes />
-          </Route>
-          <Route exact path={`${path}/unit_types`}>
-            <ListUnitTypes />
-          </Route>
-          <Route exact path={`${path}/warehouses`}>
-            <ListWarehouses />
-          </Route>
-          <Route exact path={`${path}/categories`}>
-            <ListCategories />
-          </Route>
-          <Route exact path={`${path}`}>
-            <Redirect to={`${path}/calendar`} />
-          </Route>
-        </Switch>
-      </main>
-    </div>
+          classes={{
+            paper: clsx({
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open,
+            }),
+          }}
+        >
+          <div className={classes.toolbar}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'rtl' ? (
+                <ChevronRightIcon />
+              ) : (
+                <ChevronLeftIcon />
+              )}
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            {DRAWER_ITEMS.map((elementList, index) => {
+              let itemList = elementList.map((element) => (
+                <ListItem
+                  button
+                  key={element.text}
+                  component={Link}
+                  to={element.linkTo}
+                  replace
+                >
+                  <ListItemIcon>{element.icon}</ListItemIcon>
+                  <ListItemText primary={element.text} />
+                </ListItem>
+              ));
+
+              if (index !== DRAWER_ITEMS.length - 1) {
+                itemList = itemList.concat(
+                  <Divider key={`elementlist-${index}`} />
+                );
+              }
+
+              return itemList;
+            })}
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Switch>
+            <Route exact path={`${path}/calendar`}>
+              <Calendar />
+            </Route>
+            <Route exact path={`${path}/products`}>
+              <ListProducts />
+            </Route>
+            <Route exact path={`${path}/taxes`}>
+              <ListTaxes />
+            </Route>
+            <Route exact path={`${path}/unit_types`}>
+              <ListUnitTypes />
+            </Route>
+            <Route exact path={`${path}/warehouses`}>
+              <ListWarehouses />
+            </Route>
+            <Route exact path={`${path}/categories`}>
+              <ListCategories />
+            </Route>
+            <Route exact path={`${path}`}>
+              <Redirect to={`${path}/calendar`} />
+            </Route>
+          </Switch>
+        </main>
+      </div>
+      <FullScreenDialog
+        open={openSettingsDialog}
+        closeCallback={() => setOpenSettingsDialog(false)}
+        title={t('settings.title')}
+        childComponent={<Settings />}
+      />
+    </>
   );
 };
 
