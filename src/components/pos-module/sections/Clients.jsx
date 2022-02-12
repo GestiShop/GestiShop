9a0d4ae7c46ba2b8edd1414b0c1cf13cc9ee7b51
FerranMontoliu <montoliuferran@gmail.com>
2022-02-12 21:41:31 +0100

    refactor: Deleted POS module.

--- src/components/pos-module/sections/Clients.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Clients = () => {
-  return <p>TODO</p>;
-};
-
-export default Clients;
--- src/components/pos-module/sections/Providers.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Providers = () => {
-  return <p>TODO</p>;
-};
-
-export default Providers;
