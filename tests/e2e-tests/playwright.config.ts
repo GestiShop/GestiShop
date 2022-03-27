6cdd24b55aebc4183d4e7c0942407ac89385e58a
FerranMontoliu <ferran.montoliu@students.salle.url.edu>
2022-03-28 01:26:24 +0200

    feature: Added viewport dimensions for e2e-tests.

--- tests/e2e-tests/playwright.config.ts
+++ tests/e2e-tests/playwright.config.ts
@@ -1,6 +1,12 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+  use: {
+    viewport: {
+      width: 1920,
+      height: 1080,
+    },
+  },
   timeout: 5 * 60 * 1000,
   retries: 0,
 };
