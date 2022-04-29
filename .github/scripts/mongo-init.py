3b0ebfdcebb4a31a7107c7e5e8cfc4f284280015
FerranMontoliu <ferran.montoliu@students.salle.url.edu>
2022-04-29 15:00:05 +0200

    testing: Troubleshooting mongo connection.

--- .github/scripts/mongo-init.py
+++ .github/scripts/mongo-init.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://test-user:test-password@localhost:27017/?retryWrites=true&w=majority")
-print(client.server_info())
-collection = client['gestishop']['testCollection']
-collection.insert_one({"test": "test"})
-print(collection.find_one())
+client.gestishop.command('createUser',
+                         'test-user',
+                         pwd='test-password',
+                         roles=[{'role': 'readWrite', 'db': 'gestishop'}])
+client.gestishop['testCollection'].insert_one({"test": "test"})
