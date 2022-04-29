7840dc385c2244b89d8cc4ac85b7e91c35973e8f
FerranMontoliu <ferran.montoliu@students.salle.url.edu>
2022-04-29 13:37:26 +0200

    testing: Trying to debug mongo connection.

--- tests/db-tests.py
+++ tests/db-tests.py
@@ -5,4 +5,4 @@ try:
     print(pymongo.MongoClient("mongodb://test-user:test-password@localhost:27017/gestishop?retryWrites=true&w=majority")['gestishop']['taxes'].find_one())
 except:
     print('Second try')
-    print(pymongo.MongoClient("mongodb://test-user:test-password@localhost:27017")['gestishop']['taxes'].find_one())
+    print(pymongo.MongoClient("mongodb://test-user:test-password@localhost:27017/?retryWrites=true&w=majority")['gestishop']['taxes'].find_one())
