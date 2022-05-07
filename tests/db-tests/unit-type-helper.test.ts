ce3c71d3a5030bfa4eaab0b0acbfb8b40762be90
FerranMontoliu <ferran.montoliu@students.salle.url.edu>
2022-05-07 21:58:59 +0200

    refactor: Re-wrote unit-type-helper db-test using chai.

--- tests/db-tests/unit-type-helper.test.ts
+++ tests/db-tests/unit-type-helper.test.ts
@@ -2,7 +2,6 @@
  * @jest-environment node
  */
 
-import '@testing-library/jest-dom';
 import sinon from 'sinon';
 import * as time from './utils/time';
 import {
@@ -17,10 +16,18 @@ import {
 } from './utils/database-config';
 import { DBHelperResponse, UnitType } from '../../src/model';
 import { SampleUnitType00, SampleUnitType01 } from './samples';
-import * as _ from 'lodash';
+import * as chai from 'chai';
+import chaiExclude from 'chai-exclude';
 
+// Config needed because of mongoose
 sinon.stub(time, 'setTimeout');
+
+// Configure timeout of each test
 jest.setTimeout(35000);
+
+// Configure chai and chai-exclude
+chai.use(chaiExclude);
+const expect: Chai.ExpectStatic = chai.expect;
 
 beforeAll(async () => {
   await connectDatabase();
@@ -33,19 +40,17 @@ afterAll(async () => {
 
 describe('UnitType helper', () => {
   it('Fetch all (empty)', async () => {
-    expect.assertions(1);
-
     const sampleResponse: DBHelperResponse<Array<UnitType>> = {
       result: [],
       error: null,
     };
     const response = await fetchUnitTypes();
 
-    expect(_.isMatch(response, sampleResponse)).toEqual(true);
+    expect(response) //
+      .to.deep.equal(sampleResponse);
   });
 
   it('Insert (one)', async () => {
-    expect.assertions(2);
     {
       const sampleResponse: DBHelperResponse<boolean> = {
         result: true,
@@ -54,7 +59,8 @@ describe('UnitType helper', () => {
 
       const response = await upsertUnitType(SampleUnitType00);
 
-      expect(_.isMatch(response, sampleResponse)).toEqual(true);
+      expect(response) //
+        .to.deep.equal(sampleResponse);
     }
     {
       const sampleResponse: DBHelperResponse<Array<UnitType>> = {
@@ -64,12 +70,13 @@ describe('UnitType helper', () => {
 
       const response = await fetchUnitTypes();
 
-      expect(_.isMatch(response, sampleResponse)).toEqual(true);
+      expect(response) //
+        .excludingEvery(['id'])
+        .to.deep.equal(sampleResponse);
     }
   });
 
   it('Update (one)', async () => {
-    expect.assertions(2);
     {
       const sampleResponse: DBHelperResponse<boolean> = {
         result: true,
@@ -81,7 +88,8 @@ describe('UnitType helper', () => {
 
       const response = await upsertUnitType(unitType);
 
-      expect(_.isMatch(response, sampleResponse)).toEqual(true);
+      expect(response) //
+        .to.deep.equal(sampleResponse);
     }
     {
       const sampleResponse: DBHelperResponse<Array<UnitType>> = {
@@ -91,25 +99,25 @@ describe('UnitType helper', () => {
 
       const response = await fetchUnitTypes();
 
-      expect(_.isMatch(response, sampleResponse)).toEqual(true);
+      expect(response) //
+        .excludingEvery(['id'])
+        .to.deep.equal(sampleResponse);
     }
   });
 
   it('Fetch all (with results)', async () => {
-    expect.assertions(1);
-
     const sampleResponse: DBHelperResponse<Array<UnitType>> = {
       result: [SampleUnitType01],
       error: null,
     };
     const response = await fetchUnitTypes();
 
-    expect(_.isMatch(response, sampleResponse)).toEqual(true);
+    expect(response) //
+      .excludingEvery(['id'])
+      .to.deep.equal(sampleResponse);
   });
 
   it('Fetch by id', async () => {
-    expect.assertions(1);
-
     const unitType: UnitType = SampleUnitType01;
     unitType.id = (await fetchUnitTypes()).result?.[0]?.id;
 
@@ -123,10 +131,8 @@ describe('UnitType helper', () => {
         ? await fetchUnitTypeById(unitType.id)
         : undefined;
 
-    expect(
-      response === undefined //
-        ? false
-        : _.isMatch(response, sampleResponse)
-    ).toEqual(true);
+    expect(response) //
+      .excludingEvery(['id'])
+      .to.deep.equal(sampleResponse);
   });
 });
