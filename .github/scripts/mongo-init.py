95257b2d57abb15e985afdbc32edef12f35d487c
FerranMontoliu <ferran.montoliu@students.salle.url.edu>
2022-05-09 10:31:31 +0200

    feature: Updated mongo-init.py to also drop the database.

--- .github/scripts/mongo-init.py
+++ .github/scripts/mongo-init.py
@@ -1,8 +1,20 @@
 from pymongo import MongoClient
+from pymongo.errors import OperationFailure
 
+print('Connecting to database...')
 client = MongoClient("mongodb://test-user:test-password@localhost:27017/?retryWrites=true&w=majority")
-client.gestishop.command('createUser',
-                         'test-user',
-                         pwd='test-password',
-                         roles=[{'role': 'readWrite', 'db': 'gestishop'}])
-client.gestishop['testCollection'].insert_one({"test": "test"})
+
+print('Dropping database...')
+client.gestishop.command('dropDatabase')
+
+try:
+    print('Creating user...')
+    client.gestishop.command('createUser',
+                             'test-user',
+                             pwd='test-password',
+                             roles=[{'role': 'readWrite', 'db': 'gestishop'}])
+except OperationFailure:
+    print('The user already existed.')
+
+print('Inserting test data...')
+client.gestishop['testCollection'].insert_one({})
